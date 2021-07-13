623735cb8dc9a4f77b22060db8fde9976d76c8ad
wotjd1799 <wotjd0676@gmail.com>
2021-07-13 12:18:47 +0900

    Create : Default setting
    기본적인 틀 구성

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <Contents/>
+    <Input/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/TodoHeader'
+import Contents from './components/TodoContents'
+import Input from './components/TodoInput'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Contents,
+    Input
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0px auto;
+  margin-top: 150px;
+  width: 600px;
+  height: 700px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: solid 1px black;
+  border-radius: 10px;
 }
 </style>
