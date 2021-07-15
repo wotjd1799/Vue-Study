64cacccbb13598a12985962281d92a6689cd4c76
wotjd1799 <wotjd0676@gmail.com>
2021-07-15 23:26:45 +0900

    Update : Add a example item list
    리스트 예시를 만들어 두었다

--- src/components/TodoContents.vue
+++ src/components/TodoContents.vue
@@ -1,7 +1,10 @@
 <template>
   
-  <div>
-    c
+  <div class="wrap">
+    <ul class="listWrap">
+      <li>리스트 1</li>
+      <li>리스트 2</li>
+    </ul>
   </div>
   
 </template>
@@ -13,5 +16,18 @@ export default {
 </script>
 
 <style scoped>
-  
+.wrap {
+  width:100%;
+}
+ul {
+  padding: 0;
+}
+li {
+  list-style: none;
+  font-size: 25px;
+  padding-bottom: 20px;
+  padding-left: 20px;
+  margin-bottom: 20px;
+  border-bottom: 1px solid rgb(190,190,190);
+}
 </style>
